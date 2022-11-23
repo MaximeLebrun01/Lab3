ae8a8982f898cf21a0fecc8c6b615b99698e3c28
Maxime Lebrun <maxime.lebrun.01@edu.cegeptr.qc.ca>
2022-11-23 11:24:53 -0500

    ajout employe

---
+++ Lab3/Employe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Employe
+    {
+        int matricule;
+        String nom;
+        String prenom;
+
+        public int Matricule { get => matricule; set => matricule = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+    }
+}
--- Lab3/GestionBD.cs
+++ Lab3/GestionBD.cs
@@ -1,4 +1,6 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Asn1.X509;
 using System;
 using System.Collections.Generic;
@@ -11,6 +13,62 @@ namespace Lab3
 {
     internal class GestionBD
     {
-        
+
+        MySqlConnection con;
+        ObservableCollection<Employe> employes;
+        static GestionBD gestionBD = null;
+
+        public GestionBD()
+        {
+
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326ri_eq16;Uid=2168091;Pwd=2168091;");
+            employes = new ObservableCollection<Employe>();
+        }
+
+        public static GestionBD getInstance()
+        {
+            if (gestionBD == null)
+                gestionBD = new GestionBD();
+
+            return gestionBD;
+        }
+
+        public void ajoutEmploye(int matricule, string nom, string prenom)
+        {
+            try
+            {
+                MySqlCommand commande = new MySqlCommand("p_insert_employe");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                commande.Parameters.AddWithValue("@mat", matricule);
+                commande.Parameters.AddWithValue("@nom", nom);
+                commande.Parameters.AddWithValue("@prenom", prenom);
+
+                con.Open();
+                commande.Prepare();
+                int i = commande.ExecuteNonQuery();
+
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                    con.Close();
+            }
+        }
+        private int verificationText(TextBox box, TextBlock erreur)
+        {
+            if (box.Text.Length <= 0)
+            {
+                erreur.Visibility = Visibility.Visible;
+                return 1;
+            }
+            else
+            {
+                erreur.Visibility = Visibility.Collapsed;
+                return 0;
+            }
+        }
     }
 }
