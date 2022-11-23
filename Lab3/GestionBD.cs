10cf950158dbcb265ae7949c9b256428c695b07d
Ludovic <loudo7000@gmail.com>
2022-11-23 11:22:54 -0500

    singleton

--- Lab3/GestionBD.cs
+++ Lab3/GestionBD.cs
@@ -11,6 +11,21 @@ namespace Lab3
 {
     internal class GestionBD
     {
-        
-    }
+        MySqlConnection con;
+        //ObservableCollection<maison> liste;
+        static GestionBD gestionBD = null;
+
+        public GestionBD()
+        {
+            MySqlConnection con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326ri_eq16;Uid=2168091;Pwd=2168091;");
+            //liste = new ObservableCollection<maison>();
+        }
+
+        public static GestionBD getInstance()
+        {
+            if (gestionBD == null)
+                gestionBD = new GestionBD();
+
+            return gestionBD;
+        }
 }
