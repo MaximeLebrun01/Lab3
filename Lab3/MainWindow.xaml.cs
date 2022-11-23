24a065a2e6a30979144e81b007beb75017d94cdc
Ludovic <loudo7000@gmail.com>
2022-11-23 10:19:06 -0500

    navigation + Singleton

--- Lab3/MainWindow.xaml.cs
+++ Lab3/MainWindow.xaml.cs
@@ -28,9 +28,5 @@ namespace Lab3
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
     }
 }
