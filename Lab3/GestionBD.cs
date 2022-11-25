18eb03794e7df1bf4ec47855e5cda90e5a6505f2
Maxime Lebrun <maxime.lebrun.01@edu.cegeptr.qc.ca>
2022-11-25 10:17:51 -0500

    singleton

--- Lab3/GestionBD.cs
+++ Lab3/GestionBD.cs
@@ -152,6 +152,135 @@ namespace Lab3
 
         }
 
+        public void ajoutEmploye(string matricule, string nom, string prenom)
+        {
+            try
+            {
+                MySqlCommand commande = new MySqlCommand("p_insert_employe");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                commande.Parameters.AddWithValue("@mat", matricule);
+                commande.Parameters.AddWithValue("@nom", nom);
+                commande.Parameters.AddWithValue("@prenom", prenom);
+
+                con.Open();
+                commande.Prepare();
+                int i = commande.ExecuteNonQuery();
+
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        public ObservableCollection<Employe> getNom(string nom)
+        {
+            employes.Clear();
+
+            try
+            {
+                MySqlCommand commande = new MySqlCommand("p_recherche_employe_nom");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                commande.Parameters.AddWithValue("@pnom", nom);
+
+                con.Open();
+                MySqlDataReader r = commande.ExecuteReader();
+                while (r.Read())
+                {
+                    employes.Add(new Employe()
+                    {
+                        Matricule = r.GetString(0),
+                        Nom = r.GetString(1),
+                        Prenom = r.GetString(2),
+                    });
+
+
+                }
+                r.Close();
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                    con.Close();
+            }
+
+
+            return employes;
+        }
+
+        public ObservableCollection<Employe> getPrenom(string nom)
+        {
+            employes.Clear();
+
+            try
+            {
+                MySqlCommand commande = new MySqlCommand("p_recherche_employe_prenom");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                commande.Parameters.AddWithValue("@pnom", nom);
+
+                con.Open();
+                MySqlDataReader r = commande.ExecuteReader();
+                while (r.Read())
+                {
+                    employes.Add(new Employe()
+                    {
+                        Matricule = r.GetString(0),
+                        Nom = r.GetString(1),
+                        Prenom = r.GetString(2),
+                    });
+
+
+                }
+                r.Close();
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                    con.Close();
+            }
+
+
+            return employes;
+        }
+        public int verificationText(TextBox box, TextBlock erreur)
+        {
+            if (box.Text.Length <= 0)
+            {
+                erreur.Text = "Ce champ est obligatoire";
+                erreur.Visibility = Visibility.Visible;
+                return 1;
+            }
+            else
+            {
+                erreur.Visibility = Visibility.Collapsed;
+                return 0;
+            }
+        }
+
+        public int verificationBox(ComboBox box, TextBlock erreur)
+        {
+            if (box.SelectedItem == null)
+            {
+                erreur.Visibility = Visibility.Visible;
+                return 1;
+            }
+            else
+            {
+                erreur.Visibility = Visibility.Collapsed;
+                return 0;
+            }
+        }
+
 
     }
 }
