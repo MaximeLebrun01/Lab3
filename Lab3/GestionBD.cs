75433f33d1f06dbd682a1e47ce6cfbbd8433358a
Ludovic Vendette <loudo7000@gmail.com>
2022-11-24 19:36:35 -0500

    ajout projet

--- Lab3/GestionBD.cs
+++ Lab3/GestionBD.cs
@@ -1,8 +1,10 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Asn1.X509;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +19,7 @@ namespace Lab3
 
         public GestionBD()
         {
-            MySqlConnection con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326ri_eq16;Uid=2168091;Pwd=2168091;");
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326ri_eq16;Uid=2168091;Pwd=2168091;");
             //liste = new ObservableCollection<maison>();
         }
 
@@ -28,5 +30,38 @@ namespace Lab3
 
             return gestionBD;
         }
+
+        public int ajouterProjet(projet p)
+        {
+
+
+            int retour = 0;
+            try
+            {
+                MySqlCommand commande = new MySqlCommand("p_insert_projet");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                commande.Parameters.AddWithValue("num", p.Num);
+                commande.Parameters.AddWithValue("debut", p.Debut);
+                commande.Parameters.AddWithValue("budget", p.Budget);
+                commande.Parameters.AddWithValue("descrip", p.Descrip);
+                commande.Parameters.AddWithValue("mat", p.Mat);
+
+                con.Open();
+                commande.Prepare();
+                retour = commande.ExecuteNonQuery();
+
+                con.Close();
+                return retour;
+            }
+            catch (MySqlException ex)
+            {
+                con.Close();
+                return 0;
+            }
+
+        }
+
     }
 }
